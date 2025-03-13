eb53c52df9251e8f725a2cc23281662f08879fe2
Zhenfei <406704926@qq.com>
2025-03-13 10:47:36 +0800

    Update OpenAPI2ViewModel.js

--- OpenAPI2ViewModel.js
+++ OpenAPI2ViewModel.js
@@ -1,6 +1,8 @@
 export default function openAPI2ViewModel(oas, uiMapping) {
   const viewModel = {};
 
+  // https://kubb.dev/
+  // https://github.com/readmeio/oas?tab=readme-ov-file
   // generate viewModel by oas and uiMapping
   // ...
 
